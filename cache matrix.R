fff2953614bee09a41d10e1becd91ca5791acfdc
bosky2001 <42346504+bosky2001@users.noreply.github.com>
2020-06-11 21:04:03 +0530

    Update cache matrix.R

--- cache matrix.R
+++ cache matrix.R
@@ -1,24 +1,37 @@
+#use library(matlib) to access inv(),Inverse() functions
+#function to cache matrix inverses
 makeMatrix <- function(x = matrix()) {
+        #making a null matrix to store inverse value
         t <- NULL
-        set <- function(y) {
-                mat <<- y
+        #
+        set <- function(matrix) {
+                mat <<- matrix
                 t <<- NULL
         }
+        #retrieving the matrix
         get <- function() mat
-        setinverse <- function(matrix.inverse) t <<- matrix.inverse
-        getinverse <- function() t
+        #assigning the inverse matrix
+        setinv <- function(inverse) t <<- inverse
+        #retrieving it 
+        getinv <- function() t
+        #list of all methods
         list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+             setinv = setinv,
+             getinv= getinv)
 }
+#function to retrieve inverse of matrix if in cache ,if not, this computes it 
 cacheinverse <- function(x, ...) {
-        m <- getinverse[1]
+        #returning the matrix inverse if there
+        m <- x$getinv()
+        #if m is'nt null, i.e inverse is in cache and returning the inverse
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
-        data <- get[1]
-        m <- matrix.inverse(data, ...)
-        setinverse[1]
+        #getting matrix
+        data <- x$get()
+        #finding the matrix inverse
+        m <- inv(data, ...) # even <-Inverse(data,...) and <-solve(data) %*% data
+        x$setinv(m)
         m
 }
